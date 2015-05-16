3d24e2f52cf689fec98843dafbdb658fbd0d2469
Liam Whan <liam.whan@gmail.com>
2015-05-16 14:30:18 +1000

    float fixed. before tweaking responsive

---
+++ index.php
@@ -0,0 +1,51 @@
+<html>
+
+<head>
+    <link rel="stylesheet" href="css/grid_3.css"/>
+    <link rel="stylesheet" href="css/grid_5.css"/>
+    <link rel="stylesheet" href="css/main.css"/>
+    <script src="//use.typekit.net/rfb0soz.js"></script>
+    <script>try {
+            Typekit.load();
+        } catch (e) {
+        }</script>
+    <script type="application/javascript" src="https://code.jquery.com/jquery-2.1.4.min.js"></script>
+    <script type="application/javascript" src="js/bg.js"></script>
+    <script type="application/javascript" src="js/jquery.ripples/js/jquery.ripples.js"></script>
+    <title>Liam Whan - Freelance Web Developer</title>
+<body class="bg-main">
+<header class="section group">
+    <div class="span_1_of_3 col logo-wrapper">
+        <div class="logo-text logo"><a href="#">LIAM WHAN</a></div>
+    </div>
+
+    <nav class=" col span_2_of_3 ">
+        <ul class="nav-menu ">
+            <li class="nav-item"><a href="/">HOME</a></li>
+            <li class="nav-item "><a href="#">WORK</a></li>
+            <li class="nav-item "><a href="#">HIRE ME</a></li>
+            <li class="nav-item "><a href="#">BLOG</a></li>
+            <li class="nav-item "><a href="#">CONTACT</a></li>
+
+
+        </ul>
+
+    </nav>
+
+
+</header>
+
+<main>
+
+    <section class="callout">
+        <p>freelance designer,<br/>
+            web developer<br/>
+            and all round decent bloke<br/>
+        </p>
+    </section>
+
+</main>
+
+
+</body>
+</head></html>
---
+++ js/shifty.js
@@ -0,0 +1,20 @@
+/**
+ * Created by Liam on 14/05/2015.
+ */
+
+;
+(function ($, window, document, undefined) {
+
+    var pluginName = "shifty";
+
+
+    $.extend($.fn, {
+        shifty: function (opts) {
+
+
+        }
+
+
+    });
+
+})(window.jQuery, window, document, undefined);
